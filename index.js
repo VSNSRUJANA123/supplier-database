dfbbbcf2b0b1bc4232a4e287effd7342a3edb8c5
VSNSRUJANA123 <srujanadvsn@gmail.com>
2024-09-10 13:38:32 +0530

    backend

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// https://dribbble.com/shots/14864303-Inventory-management-Mobile-app-Customer-Part
 const express = require("express");
 const mysql = require("mysql2");
 const dotenv = require("dotenv");
@@ -7,40 +8,21 @@ app.use(express.json());
 const cors = require("cors");
 app.use(cors());
 
-let db;
+const db = mysql.createPool({
+  host: "185.199.52.156",
+  user: "inventoryapp",
+  database: "inventory",
+  password: "iLKodMll10XJhBi1lgTp",
+});
 
-function handleDisconnect() {
-  db = mysql.createConnection({
-    host: "brhbm2qdamv5vvlodwd6-mysql.services.clever-cloud.com",
-    user: "udznwgiob6lngucm",
-    password: "ONVXuK29rrbzz8AjXAOF",
-    database: "brhbm2qdamv5vvlodwd6",
-    port: 3306,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-  });
-
-  db.connect((err) => {
-    if (err) {
-      console.error("Error connecting to the database:", err);
-      setTimeout(handleDisconnect, 2000);
-    } else {
-      console.log("Connected to the MySQL database.");
-    }
-  });
-
-  db.on("error", (err) => {
-    console.error("Database error:", err);
-    if (err.code === "PROTOCOL_CONNECTION_LOST" || err.fatal) {
-      handleDisconnect();
-    } else {
-      throw err;
-    }
-  });
-}
-
-handleDisconnect();
+db.getConnection((err, connection) => {
+  if (err) {
+    console.error("Error connecting to the database:", err);
+    return;
+  }
+  console.log("Connected to MySQL database!");
+  connection.release();
+});
 
 app.get("/contacts", (req, res) => {
   const query = "select * from contacts";
@@ -67,8 +49,16 @@ app.get("/contacts/:id", (req, res) => {
   });
 });
 app.post("/add-contact", (req, res) => {
-  const { id, firstname, lastname, email, phonenumber, company_name, address } =
-    req.body;
+  const {
+    id,
+    firstname,
+    lastname,
+    email,
+    phonenumber,
+    job_title,
+    company_name,
+    address,
+  } = req.body;
 
   if (
     (!id,
@@ -76,17 +66,18 @@ app.post("/add-contact", (req, res) => {
       !lastname ||
       !email ||
       !phonenumber ||
+      !job_title ||
       !company_name ||
       !address)
   ) {
     return res.status(400).json({ error: "All fields are required" });
   }
 
-  const query = `INSERT INTO contacts (firstname, lastname,email, phonenumber, company_name, address) 
-                   VALUES (?, ?, ?, ?, ?,?)`;
+  const query = `INSERT INTO contacts (firstname, lastname,email, phonenumber, job_title,company_name, address) 
+                   VALUES (?, ?, ?, ?,?, ?,?)`;
   db.query(
     query,
-    [firstname, lastname, email, phonenumber, company_name, address],
+    [firstname, lastname, email, phonenumber, job_title, company_name, address],
     (err, result) => {
       if (err) {
         console.error("Error inserting data:", err);
@@ -99,7 +90,58 @@ app.post("/add-contact", (req, res) => {
     }
   );
 });
-
+app.put("/update-contact/:id", (req, res) => {
+  const { id } = req.params;
+  const {
+    firstname,
+    lastname,
+    email,
+    phonenumber,
+    job_title,
+    company_name,
+    address,
+  } = req.body;
+  const query = `update contacts set firstname=?,lastname=?,email=?,
+    phonenumber=?,
+    job_title=?,
+    company_name=?,
+    address=? where id=?`;
+  db.query(
+    query,
+    [
+      firstname,
+      lastname,
+      email,
+      phonenumber,
+      job_title,
+      company_name,
+      address,
+      id,
+    ],
+    (err, result) => {
+      if (err) {
+        return res.status(500).json({ error: "Failed to update column" });
+      }
+      if (result.affectedRows === 0) {
+        return res.status(404).json({ message: "Item not found" });
+      }
+      res.status(200).json({ message: "Item updated Successfully" });
+    }
+  );
+});
+app.delete("/delete-contact/:id", (req, res) => {
+  const { id } = req.params;
+  const query = `delete from contacts where id=? `;
+  db.query(query, [id], (err, result) => {
+    if (err) {
+      return res.status(500).send({ error: "failed to delete" });
+    }
+    if (result.affectedRows === 0) {
+      return res.status(500).send({ error: "data not found" });
+    }
+    res.status(200).json({ message: "Delete data successfully" });
+  });
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
