1ce15a945dbc544b327505bb1bec69f715e9132d
VSNSRUJANA123 <srujanadvsn@gmail.com>
2024-10-06 12:29:04 +0530

    backend

--- index.js
+++ index.js
@@ -10,14 +10,21 @@ const orders = require("./routers/orders");
 const orderDetails = require("./routers/orderDetails");
 const orderstatus = require("./routers/orderStatus");
 dotenv.config();
-app.use(express.json());
 const cors = require("cors");
+app.use(express.json());
+
 app.use("/employees", employeesRoutes);
 app.use("/suppliers", suppliersRoutes);
 app.use("/products", productRoute);
 app.use("/orders", orders);
 app.use("/orderDetails", orderDetails);
 app.use("/orderstatus", orderstatus);
+app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
